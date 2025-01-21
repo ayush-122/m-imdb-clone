51120e4325832860d798d91ee517caf222f90146
Ayush Singh <ayush.singh@gamemano.in>
2025-01-21 15:59:31 +0530

    fixed/remove add to Favourites

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -47,7 +47,7 @@ const Cards = ({ movie }) => {
   };
 
   return (
-    <>
+    <div className="cards-wrapper">
       {isLoading ? (
         <div className="cards">
           <SkeletonTheme color="#202020" highlightColor="#444">
@@ -55,33 +55,43 @@ const Cards = ({ movie }) => {
           </SkeletonTheme>
         </div>
       ) : (
-        <div className="cards-container">
-          <Link to={`/movie/${movie.id}`} style={{ textDecoration: "none", color: "white" }}>
-            <div className="cards">
-              <img className="cards__img" src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`} alt={movie?.original_title || "Movie Poster"} />
-              <div className="cards__overlay">
-                <div className="card__title">{movie?.original_title || ""}</div>
-                <div className="card__runtime">
-                  {movie?.release_date || ""}
-                  <span className="card__rating">
-                    {movie?.vote_average || ""}
-                    <i className="fas fa-star" />
-                  </span>
+        <>
+          <div className="cards-container">
+            <Link to={`/movie/${movie.id}`} style={{ textDecoration: "none", color: "white" }}>
+              <div className="cards">
+                <img
+                  className="cards__img"
+                  src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
+                  alt={movie?.original_title || "Movie Poster"}
+                />
+                <div className="cards__overlay">
+                  <div className="card__title">{movie?.original_title || ""}</div>
+                  <div className="card__runtime">
+                    {movie?.release_date || ""}
+                    <span className="card__rating">
+                      {movie?.vote_average || ""}
+                      <i className="fas fa-star" />
+                    </span>
+                  </div>
+                  <div className="card__description">
+                    {movie?.overview?.slice(0, 118) + "..." || ""}
+                  </div>
                 </div>
-                <div className="card__description">{movie?.overview?.slice(0, 118) + "..." || ""}</div>
-                {/* Add/Remove Favorites Button */}
-                <button
-                  onClick={isFavorite ? removeFromFavorites : addToFavorites}
-                  className={`favorite-btn ${isFavorite ? "favorite-btn--active" : ""}`}
-                >
-                  {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
-                </button>
               </div>
-            </div>
-          </Link>
-        </div>
+            </Link>
+          </div>
+          {/* Add/Remove Favorites Button */}
+          <div className="favorite-btn-container">
+            <button
+              onClick={isFavorite ? removeFromFavorites : addToFavorites}
+              className={`favorite-btn ${isFavorite ? "favorite-btn--active" : ""}`}
+            >
+              {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+            </button>
+          </div>
+        </>
       )}
-    </>
+    </div>
   );
 };
 
